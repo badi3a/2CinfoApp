914c4b1cf530915dba6667cda2037c815c40eeeb
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-12-28 18:32:39 +0100

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
